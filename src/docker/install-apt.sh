efa5195ba085a341d81fd8b88f0dc14f091f54f6
danlooo <loosdaniel@gmail.com>
2022-10-07 18:07:06 +0200

    Refactor files

--- src/docker/install-apt.sh
+++
@@ -1,3 +0,0 @@
-apt-get update
-
-apt-get install -y python3 python3.8-venv
--- src/docker/install.R
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env Rscript
-
-options(repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/__linux__/focal/2022-08-01+Y3JhbiwyOjQ1MjYyMTU7RTY0MEEyRTM"))
-
-cran_packages <- c("targets", "reticulate", "spotifyr", "styler", "ggnewscale", "ggrepel", "ggpubr", "ggbeeswarm", "broom", "Hmisc")
-
-install.packages(cran_packages)
-
-reticulate::py_install(c("pandas", "numpy", "tensorflow", "keras", "sklearn"))
